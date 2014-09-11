777802d46c5881bbae1fa1521f4da9da548eb09c
Will Boyd <will@codersblock.com>
2014-09-11 16:51:39 -0700

    Still working on links page.

--- routes/views/coming.js
+++ routes/views/coming.js
@@ -3,7 +3,6 @@ var keystone = require('keystone');
 exports = module.exports = function(req, res) {
 	// setup
 	var view = new keystone.View(req, res);
-	res.locals.data = {};
 
 	// nothing
 	view.on('init', function(next) {
--- routes/views/links.js
+++ routes/views/links.js
@@ -3,46 +3,54 @@ var keystone = require('keystone'),
 
 exports = module.exports = function(req, res) {
 	// constants
-	var TIMEZONE = 'America/Vancouver';
-	var DATE_FORMAT = 'YYYY-M-D';
+	var TIMEZONE = 'America/Vancouver';			// Seattle local time
+	var DATE_DATA_FORMAT = 'YYYY-M-D';			// ex: 2014-7-25
+	var DATE_CURRENT_FORMAT = 'MMMM Do, YYYY';	// ex: July 25th, 1980
+	var DATE_NAV_FORMAT = 'MMMM Do';			// ex: July 25th
 	var HOME_PATH = '/home';
 	var SNEAK_PEAK_HOUR = 18;
 
 	// setup
 	var view = new keystone.View(req, res);
-	res.locals.data = {
-		links: []
-	};
+	res.locals.showDateNav = true;
 
 	// get the present moment, timezone shifted
 	function getPresentMoment() {
-		return moment.tz(Date.now(), TIMEZONE)
+		return moment.tz(Date.now(), TIMEZONE);
 	}
 
 	// create a new moment, offset by some number of days
 	function createOffsetMoment(originalMoment, dayOffset) {
-		return moment(originalMoment).add(dayOffset, 'd')
+		return moment(originalMoment).add(dayOffset, 'd');
 	}
 
 	// calculate dates
 	view.on('init', function(next) {
-		// default to the present date
+		// default to present day
 		var currentMoment = getPresentMoment();
 
-		// give incoming date param a chance to override, if valid
+		// give incoming date param a chance to override
 		if (req.params.date) {
-			var incomingMoment = moment(req.params.date, DATE_FORMAT);
-			if (incomingMoment.isValid()) {
-				currentMoment = incomingMoment;
-			} else {
-				// error
-				next('nope');
+			currentMoment = moment(req.params.date, DATE_DATA_FORMAT);
+			if (!currentMoment.isValid()) {
+				// TODO: render 404 page
+				res.status(404);
 			}
 		}
 
-		res.locals.data.currentDate = currentMoment.format(DATE_FORMAT);
-		res.locals.data.prevDate = createOffsetMoment(currentMoment, -1).format(DATE_FORMAT);
-		res.locals.data.nextDate = createOffsetMoment(currentMoment, 1).format(DATE_FORMAT);
+		// current date
+		res.locals.currentDate = currentMoment.format(DATE_DATA_FORMAT);
+		res.locals.currentDateDisplay = (req.route.path === HOME_PATH) ? 'Today' : currentMoment.format(DATE_CURRENT_FORMAT);
+
+		// previous date
+		var prevMoment = createOffsetMoment(currentMoment, -1);
+		res.locals.prevDate	= prevMoment.format(DATE_DATA_FORMAT);
+		res.locals.prevDateDisplay = prevMoment.format(DATE_NAV_FORMAT);
+
+		// next date
+		var nextMoment = createOffsetMoment(currentMoment, 1);
+		res.locals.nextDate	= nextMoment.format(DATE_DATA_FORMAT);
+		res.locals.nextDateDisplay = nextMoment.format(DATE_NAV_FORMAT);
 
 		next();
 	});
@@ -50,10 +58,10 @@ exports = module.exports = function(req, res) {
 	// load links
 	view.on('init', function(next) {
 		keystone.list('Link').model.find()
-			.where('publish', res.locals.data.currentDate)
+			.where('publish', res.locals.currentDate)
 			.limit(6)
 			.exec(function(err, results) {
-				res.locals.data.links = results;
+				res.locals.links = results;
 				next(err);
 			});
 	});
@@ -62,11 +70,11 @@ exports = module.exports = function(req, res) {
 	view.on('init', function(next) {
 		if (req.route.path === HOME_PATH && getPresentMoment().hour() >= SNEAK_PEAK_HOUR) {
 			keystone.list('Link').model.find()
-				.where('publish', res.locals.data.nextDate)
+				.where('publish', res.locals.nextDate)
 				.limit(1)
 				.exec(function(err, results) {
 					if (results.length > 0) {
-						res.locals.data.links.push(results[0]);
+						res.locals.links.push(results[0]);
 					}
 					next(err);
 				});
