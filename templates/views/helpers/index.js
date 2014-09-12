105b5b029bf5ca67fd57b24e1a8a04f6b9f496e4
Will Boyd <will@codersblock.com>
2014-09-12 15:09:11 -0700

    Helpers for nav URLs.

--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -7,6 +7,15 @@ module.exports = function() {
 		return moment(date).format(format);
 	};
 
+	_helpers.formatCurrentDate = function(date, format, isHome) {
+		return (isHome ? 'Today' : _helpers.formatDate(date, format));
+	};
+
+	_helpers.getNavUrl = function(date) {
+		var today = moment.tz('America/Vancouver').startOf('d').format('YYYY-MM-DD');
+		return ((date === today) ? '/home' : 'archives/' + date);
+	};
+
     _helpers.formatAuthor = function(author) {
         return 'by ' + author;
     };
