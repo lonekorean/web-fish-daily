dc03605507707a73db2e676539ce23cfaa7a6339
Will Boyd <will@codersblock.com>
2014-09-19 13:11:48 -0700

    Countdown clean-up and rounding.

--- scripts/site.js
+++ scripts/site.js
@@ -6,40 +6,51 @@ $(function() {
 	function startCountdown() {
 		$('.time').each(function() {
 			var $time = $(this);
-			var interval = parseInt($time.attr('data-interval'), 10);
-			var endTime = Math.ceil(Date.now() / 1000) + interval;
-			setCountdown($time, endTime);
-			setInterval(updateCountdown.bind(undefined, $time, endTime), 1000);
+			var endSeconds = getCurrentSeconds() + parseInt($time.attr('data-seconds'), 10);
+			displayCountdown($time, endSeconds);
+			setInterval(updateCountdown.bind(undefined, $time, endSeconds), 1000);
 		});
 	}
 
-	function setCountdown($time, endTime) {
-		var interval = endTime - Math.ceil(Date.now() / 1000);
-		$time.text(formatCountdown(interval));
-	}
-
-	function formatCountdown(interval) {
-		var date = new Date(interval * 1000);
-		var hours = date.getUTCHours();
-		var minutes = date.getUTCMinutes();
-		var seconds = date.getUTCSeconds();
-
-		if (hours) {
-			return hours + ' hour' + (hours > 1 ? 's' : '');
-		} else if (minutes) {
-			return minutes + ' minute' + (minutes > 1 ? 's' : '');
-		} else if (seconds) {
-			return seconds + ' seconds' + (seconds > 1 ? 's' : '');
-		}
-	}
-
-	function updateCountdown($time, endTime) {
-		if (Math.ceil(Date.now() / 1000) < endTime) {
-			setCountdown($time, endTime);
+	function updateCountdown($time, endSeconds) {
+		if (getCurrentSeconds() < endSeconds) {
+			displayCountdown($time, endSeconds);
 		} else {
 			// done
 			location.reload(true);
 		}
+	}
+
+	function displayCountdown($time, endSeconds) {
+		var secondsRemaining = endSeconds - getCurrentSeconds();
+		$time.text(formatCountdown(secondsRemaining));
+	}
+
+	function formatCountdown(secondsRemaining) {
+		var date = new Date(Math.max(secondsRemaining, 0) * 1000);
+		var hours = date.getUTCHours();
+		var minutes = date.getUTCMinutes();
+		var seconds = date.getUTCSeconds();
+
+		var value, unit;
+		if (hours) {
+			// rounded up
+			value = hours + (minutes > 0 ? 1 : 0);
+			unit = 'hour';
+		} else if (minutes) {
+			// rounded up
+			value = minutes + (seconds > 0 ? 1 : 0);
+			unit = 'minute';
+		} else {
+			value = seconds;
+			unit = 'second';
+		}
+
+		return value + ' ' + unit + (value !== 1 ? 's' : '');
+	}
+
+	function getCurrentSeconds() {
+		return Math.ceil(Date.now() / 1000);
 	}
 
 	init();
